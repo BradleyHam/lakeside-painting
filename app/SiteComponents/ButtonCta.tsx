abdd887bb3fd74d21ffa907bc5154506e358e609
bradleyham <bradleyjamesham@gmail.com>
2024-09-12 16:13:47 +0700

    fix hero ts error

--- app/SiteComponents/ButtonCta.tsx
+++ app/SiteComponents/ButtonCta.tsx
@@ -1,13 +1,17 @@
 import { LuPaintbrush } from 'react-icons/lu';
 import Link from 'next/link';
 
-const ButtonCta = ({ text, type }: { text: string, type: number }) => {
-  return (
-    <Link href="/bookingPage" className={`my-auto uppercase shadow-xl rounded font-bold tracking-wider text-xs transition duration-300 ease-in-out flex items-center py-4 px-6 ${type === 1 ? 'text-primary bg-accent-1' : 'bg-primary text-white'}`}>
-      <LuPaintbrush size={20} style={{ strokeWidth: '2px' }} />
-      <span className='ml-2'>{text}</span>
-    </Link>
-  );
-};
+interface ButtonCtaProps {
+    text: string;
+    type: number;
+    className?: string; // Add this line
+}
 
-export default ButtonCta;
+export default function ButtonCta({ text, type, className }: ButtonCtaProps) {
+    return (
+        <Link href="/bookingPage" className={`my-auto uppercase shadow-xl rounded font-bold tracking-wider text-xs transition duration-300 ease-in-out flex items-center py-4 px-6 ${type === 1 ? 'text-primary bg-accent-1' : 'bg-primary text-white'} ${className || ''}`}>
+            <LuPaintbrush size={20} style={{ strokeWidth: '2px' }} />
+            <span className='ml-2'>{text}</span>
+        </Link>
+    );
+};
